b914d68a78de1f798e4a2a876bf97c564a041c06
code dragon <code4allschool@outlook.com>
2024-06-11 15:11:28 -0300

    bala

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Shoot : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
-  
+    [SerializeField] float speed = 10f;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-       
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * speed * Time.deltaTime, ForceMode.Impulse);
     }
 
     // Update is called once per frame
